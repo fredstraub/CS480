f82fb7d35f712595d8d21c0b5398595be56f77c8
Fred Straub <fredstraub22@gamil.com>
2022-05-05 11:32:08 -0700

    update

---
+++ Hotel.sol
@@ -0,0 +1,6 @@
+// Final Project for CS 480 at Cal Poly Humboldt
+// by: Fred Straub
+// last modified: 5-4-2022
+// Inspired by tutorial at https://www.loginradius.com/blog/engineering/guest-post/ethereum-smart-contract-tutorial/
+
+pragma solidity ^0.5.16;
